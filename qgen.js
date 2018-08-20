7f9effc7993a2366a62ab37db60909588743cdf5
Saransh Gupta <35869697+gupta-saransh@users.noreply.github.com>
2018-08-20 20:09:08 +0530

    Add files via upload

---
+++ qgen.js
@@ -0,0 +1,68 @@
+var check=0;
+var x=1;
+  $(document).ready(function() {
+      function getNewQuote() {
+          $.ajax({
+              url: 'http://api.forismatic.com/api/1.0/',
+              jsonp: 'jsonp',
+              dataType: 'jsonp',
+              data: {
+                  method: 'getQuote',
+                  lang: 'en',
+                  format: 'jsonp'
+              },
+              success: function(response) {
+                  if (response.quoteAuthor == "") {
+                      getNewQuote();
+                  } else {
+                      document.getElementById("quote").textContent = "";
+                      document.getElementById('name').innerHTML = "";
+
+                      var i = 0;
+                      var flag = 0;
+                      var txt = response.quoteText; /* The quote text */
+                      var speed = 25; /* The speed/duration of the effect in milliseconds */
+                      function typeWriter() {
+
+                          if (i < txt.length) {
+
+                              document.getElementById("quote").innerHTML += txt.charAt(i);
+
+                              i++;
+                              setTimeout(typeWriter, speed);
+                              if (i == txt.length) {
+                                  flag = 1;
+
+                              }
+                          }
+                          if (flag == 1) {
+
+                              document.getElementById('name').innerHTML = "- " + response.quoteAuthor;
+                              flag = 0;
+                              check=1;
+                              console.log(check);
+
+                          }
+
+                      }
+                      typeWriter();
+
+
+
+                      $(".twitter-share-button").attr("href", "https://twitter.com/intent/tweet?text=" + response.quoteText + "- " + response.quoteAuthor);
+                  }
+              }
+
+
+          });
+      }
+      document.getElementById('nextq').addEventListener('click', function() {
+          if(check==1 || x==1)
+          { check=0;
+            getNewQuote();
+            x=0;
+          }
+
+      });
+
+  });
